fd5458440597ffacecba978af24433cba91811a0
Yuri Khrustalev <yuri.khrustalev@dataart.com>
2012-09-03 23:59:56 +0400

    mock for server

---
+++ server/app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+//
+var  server = http.createServer(app)
+    , io = require('socket.io').listen(server);
+//
+
+app.configure(function(){
+//  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hjs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+//
+server.listen(3000);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+    socket.on('ping', function(data){
+        console.log('pong');
+        socket.emit('pong', { hello: 'world' });
+
+    })
+});
