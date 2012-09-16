9252a3c1d4cfbc22f65b054c83e92cd795a9292e
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2012-09-16 14:08:28 +0400

    added build js spec

---
+++ server/public/javascripts/app.build.js
@@ -0,0 +1,21 @@
+({
+  appDir: "../",
+  baseUrl: "javascripts",
+  dir: "../../build",
+  outDir: "../../build",
+  paths: {
+    'jquery' : 'lib/jquery',
+    'underscore' : 'lib/underscore',
+    'backbone' : 'lib/backbone',
+    'backboneModelBinder' : 'lib/Backbone.ModelBinder',
+    'backboneValidation' : 'lib/backbone-validation-amd',
+    'hogan' : 'lib/template-2.0.0',
+    'io' : 'lib/socket.io',
+    'json2' : 'lib/json2'
+  },
+  modules: [
+    {
+      name: "config"
+    }
+  ]
+})
