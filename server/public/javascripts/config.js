4d7d83b08f121ee8043444bf187aa32dc66e0fab
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2012-09-27 22:35:51 +0400

    formating

--- server/public/javascripts/config.js
+++ server/public/javascripts/config.js
@@ -4,41 +4,44 @@
 require.config({
 
   paths: {
-    json2: "lib/json2",
-    jquery: "lib/jquery",
-    underscore: "lib/underscore",
-    backbone: "lib/backbone",
-    backboneValidation: "lib/backbone-validation-amd",
+    json2              : "lib/json2",
+    jquery             : "lib/jquery",
+    underscore         : "lib/underscore",
+    backbone           : "lib/backbone",
+    backboneValidation : "lib/backbone-validation-amd",
     backboneModelBinder: "lib/Backbone.ModelBinder",
-    hogan: "lib/template-2.0.0",
-    io: "lib/socket.io"
+    hogan              : "lib/template-2.0.0",
+    io                 : "lib/socket.io"
   },
 
   shim: {
     hogan: {
-      exports: 'Hogan'
+      exports: "Hogan"
     },
-    io : {
-      exports: 'io'
+
+    io: {
+      exports: "io"
     },
+
     underscore: {
       exports: "_"
     },
+
     backbone: {
-      deps: ["underscore", "jquery"],
+      deps   : ["underscore", "jquery"],
       exports: "Backbone"
     },
+
     backboneModelbinding: {
-      deps: ["backbone", 'underscore' ]
+      deps: ["backbone", "underscore" ]
     }
   }
 
 });
 
 require([
-  'router',
-  'json2',
-  'util/ajax'
-], function(Router) {
+  "router",
+  "json2"
+], function (Router) {
   Router.initialize();
 });
