f64e1b3a5fe18ed107af3d5fdd758981694da9bf
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2012-10-10 22:33:42 +0400

    about page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,10 @@
-var Deck = require("../deck");
-
 exports.index = function (req, res) {
   res.render('index', {});
+};
+
+exports.about = function (req, res) {
+  res.render('about', {
+    version: "0.0.1",
+    process: process
+  });
 };
