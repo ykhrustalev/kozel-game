21675ba6cd99c0c1cce6a9a8dcf92baaddbbd065
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2012-10-14 02:04:55 +0400

    cards weights and beat function

--- server/deck.js
+++ server/deck.js
@@ -1,5 +1,55 @@
 var _ = require('underscore')._;
 
+var suites , types, orderTable;
+
+suites = {
+  Spades  : {id: 's'},
+  Hearts  : {id: 'h'},
+  Diamonds: {id: 'd'},
+  Clubs   : {id: 'c'}
+};
+
+types = {
+  Queen: {id: 'Q', score: 3},
+  Jack : {id: 'J', score: 2},
+  King : {id: 'K', score: 4},
+  Ace  : {id: 'A', score: 11},
+  T10  : {id: '10', score: 10},
+  T9   : {id: '9', score: 0},
+  T8   : {id: '8', score: 0},
+  T7   : {id: '7', score: 0}
+};
+
+orderTable = {
+
+  trumpTypes: [
+    types.T7.id,
+    types.Queen.id,
+    types.Jack.id,
+    types.Ace.id,
+    types.T10.id,
+    types.King.id,
+    types.T9.id,
+    types.T8.id
+  ],
+
+  trumpSuites: [
+    suites.Clubs.id,
+    suites.Spades.id,
+    suites.Hearts.id,
+    suites.Diamonds.id
+  ],
+
+  nonTrumpsTypes: [
+    types.Ace.id,
+    types.T10.id,
+    types.King.id,
+    types.T9.id,
+    types.T8.id,
+    types.T7.id
+  ]
+};
+
 var Deck = function () {
 
   var self = this;
@@ -7,14 +57,17 @@ var Deck = function () {
   self._initialDeck = {};
 
   // create initial deck
-  _.each(self.Suites, function (suite) {
-    _.each(self.Types, function (type) {
+  _.each(suites, function (suite) {
+    _.each(types, function (type) {
       var id = self.cardIdFor(suite, type);
       self._initialDeck[id] = {
-        id   : id,
-        suite: suite,
-        type : type,
-        score: type.score
+        id     : id,
+        suite  : suite,
+        type   : type,
+        score  : type.score,
+        isTrump: suite.id === suites.Clubs.id
+          || type.id === types.Queen.id
+          || type.id === types.Jack.id
       };
     });
   });
@@ -22,9 +75,13 @@ var Deck = function () {
 
 Deck.prototype = {
 
-  //TODO: remove
-  getScore: function (cardId) {
-    return this._initialDeck[cardId].score;
+  getScore: function () {
+    var score = 0
+      , deck = this._initialDeck;
+    for (var i = 0, len = arguments.length; i < len; i++) {
+      score += deck[arguments[i]].score
+    }
+    return score;
   },
 
   // TODO: unit test
@@ -32,23 +89,9 @@ Deck.prototype = {
     return _.clone(this._initialDeck[cardId]);
   },
 
-  Suites: {
-    Spades  : {id: 's'},
-    Hearts  : {id: 'h'},
-    Diamonds: {id: 'd'},
-    Clubs   : {id: 'c'}
-  },
+  Suites: suites,
 
-  Types     : {
-    Queen: {id: 'Q', score: 3},
-    Jack : {id: 'J', score: 2},
-    King : {id: 'K', score: 4},
-    Ace  : {id: 'A', score: 11},
-    T10  : {id: '10', score: 10},
-    T9   : {id: '9', score: 0},
-    T8   : {id: '8', score: 0},
-    T7   : {id: '7', score: 0}
-  },
+  Types     : types,
 
   // TODO: unit test
   _randomize: function (deck) {
@@ -68,9 +111,49 @@ Deck.prototype = {
    * @param {String} type
    * @return {String}
    */
-    //TODO: rename to cidFor
+  //TODO: rename to cidFor
   cardIdFor: function (suite, type) {
     return suite.id + '-' + type.id;
+  },
+  cidFor: function (suite, type) {
+    return suite.id + '-' + type.id;
+  },
+
+
+  beats: function (attackCid, defendCid) {
+    var a = this.getCard(attackCid)
+      , d = this.getCard(defendCid)
+      , nt = orderTable.nonTrumpsTypes
+      , tt = orderTable.trumpTypes
+      , ts = orderTable.trumpSuites
+      , ttA = tt.indexOf(a.type.id)
+      , ttD = tt.indexOf(d.type.id)
+      , ntA = nt.indexOf(a.type.id)
+      , ntD = nt.indexOf(d.type.id)
+      , tsA = ts.indexOf(a.suite.id)
+      , tsD = ts.indexOf(d.suite.id);
+
+    if (a.isTrump && d.isTrump) {
+      if (ttA === ttD) {
+        return tsA < tsD;
+      } else {
+        return ttA < ttD;
+      }
+    } else if (a.isTrump && !d.isTrump) {
+      return true;
+    } else if (!a.isTrump && d.isTrump) {
+      return false;
+    } else if (!a.isTrump && !d.isTrump) {
+      if (tsA !== tsD) {
+        return false;
+      } else {
+        return ntA < ntD;
+      }
+    } else {
+      var message = "could not compare: a: " + attackCid + " d: " + defendCid;
+      console.warn(message);
+      throw new Error(message);
+    }
   },
 
   isTrump    : function (cardId) {
@@ -83,7 +166,7 @@ Deck.prototype = {
   // TODO: unit test, move to derived class
   sortedCards: function (cards, startCardId) {
     var self = this,
-      startCard = startCardId ? self.getCard(startCardId): null
+      startCard = startCardId ? self.getCard(startCardId) : null
       , sorted = {
         suite    : [],
         trumps   : [],
--- server/tests/test-deck.js
+++ server/tests/test-deck.js
@@ -19,5 +19,79 @@ module.exports = {
   testCardIdFor: function (test) {
     test.equals(deck.cardIdFor(deck.Suites.Diamonds, deck.Types.Ace), "d-A");
     test.done();
+  },
+
+  beats: function (test) {
+    //TODO: complete me
+    // nontrumps:
+    // - sequence
+
+
+    var spades = deck.Suites.Spades
+      , hearts = deck.Suites.Hearts
+      , diamonds = deck.Suites.Diamonds
+      , clubs = deck.Suites.Clubs
+      , Q = deck.Types.Queen
+      , J = deck.Types.Jack
+      , K = deck.Types.King
+      , A = deck.Types.Ace
+      , T10 = deck.Types.T10
+      , T9 = deck.Types.T9
+      , T8 = deck.Types.T8
+      , T7 = deck.Types.T7;
+
+    function _beats(s1, t1, s2, t2, ok) {
+      var result = deck.beats(deck.cidFor(s1, t1), deck.cidFor(s2, t2));
+      test.ok(ok ? result : !result, s1.id + "-" + t1.id + " should " + (ok ? "" : " not") + " beat " + s2.id + "-" + t2.id);
+    }
+
+    function beats(s1, t1, s2, t2) {
+      _beats(s1, t1, s2, t2, true);
+    }
+
+    function beatsNot(s1, t1, s2, t2) {
+      _beats(s1, t1, s2, t2, false);
+    }
+
+    beatsNot(diamonds, A, diamonds, Q);
+    beatsNot(diamonds, A, diamonds, J);
+    beats(diamonds, A, diamonds, T10);
+    beats(diamonds, A, diamonds, K);
+    beats(diamonds, A, diamonds, T9);
+    beats(diamonds, A, diamonds, T8);
+    beats(diamonds, A, diamonds, T7);
+
+    beatsNot(diamonds, A, clubs, T7);
+    beatsNot(diamonds, A, hearts, T10);
+    beatsNot(diamonds, A, spades, T10);
+
+    beatsNot(diamonds, T7, diamonds, A);
+    beatsNot(diamonds, T7, diamonds, T10);
+    beatsNot(diamonds, T7, diamonds, K);
+    beatsNot(diamonds, T7, diamonds, T9);
+    beatsNot(diamonds, T7, diamonds, T8);
+    beatsNot(diamonds, T7, clubs, T7);
+
+    beats(diamonds, J, diamonds, A);
+    beats(diamonds, J, spades, A);
+    beats(diamonds, J, hearts, A);
+    beats(diamonds, J, clubs, A);
+    beats(diamonds, J, clubs, T10);
+    beats(diamonds, J, clubs, K);
+    beats(diamonds, J, clubs, T9);
+    beats(diamonds, J, clubs, T8);
+    beatsNot(diamonds, J, clubs, T7);
+    beatsNot(diamonds, J, clubs, Q);
+
+    beats(clubs, Q, spades, Q);
+    beats(clubs, Q, hearts, Q);
+    beats(clubs, Q, diamonds, Q);
+
+    beatsNot(spades, Q, clubs, Q);
+    beats(spades, Q, hearts, Q);
+    beats(spades, Q, diamonds, Q);
+    beats(clubs, Q, clubs, J);
+
+    test.done();
   }
 };
